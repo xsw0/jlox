8daf218bc8e22c1eaa52182a157d412d06e4c940
xsw0 <xsw.mail.0@gmail.com>
2021-02-19 14:07:45 +0800

    statement

--- src/com/craftinginterpreters/lox/Expr.java
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -5,19 +5,15 @@ import java.util.List;
 abstract class Expr {
     interface Visitor<R> {
         R visitBinaryExpr(Binary expr);
-
         R visitGroupingExpr(Grouping expr);
-
         R visitLiteralExpr(Literal expr);
-
         R visitUnaryExpr(Unary expr);
     }
-
     static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
+              this.left = left;
+              this.operator = operator;
+              this.right = right;
         }
 
         @Override
@@ -29,10 +25,9 @@ abstract class Expr {
         final Token operator;
         final Expr right;
     }
-
     static class Grouping extends Expr {
         Grouping(Expr expression) {
-            this.expression = expression;
+              this.expression = expression;
         }
 
         @Override
@@ -42,10 +37,9 @@ abstract class Expr {
 
         final Expr expression;
     }
-
     static class Literal extends Expr {
         Literal(Object value) {
-            this.value = value;
+              this.value = value;
         }
 
         @Override
@@ -55,11 +49,10 @@ abstract class Expr {
 
         final Object value;
     }
-
     static class Unary extends Expr {
         Unary(Token operator, Expr right) {
-            this.operator = operator;
-            this.right = right;
+              this.operator = operator;
+              this.right = right;
         }
 
         @Override
--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -26,6 +28,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -51,12 +54,14 @@ public class Lox {
 //        }
 
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
+        List<Stmt> statements = parser.parse();
 
         // Stop if there was a syntax error.
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+//        System.out.println(new AstPrinter().print(expression));
+
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
@@ -76,5 +81,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
