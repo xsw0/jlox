360ae2070db8cb852d02035d6b2233d4021266a0
xsw0 <xsw.mail.0@gmail.com>
2021-02-20 15:07:10 +0800

    Statements and State

--- src/com/craftinginterpreters/lox/Expr.java
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -4,16 +4,34 @@ import java.util.List;
 
 abstract class Expr {
     interface Visitor<R> {
+        R visitAssignExpr(Assign expr);
         R visitBinaryExpr(Binary expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitUnaryExpr(Unary expr);
+        R visitVariableExpr(Variable expr);
     }
+
+    static class Assign extends Expr {
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
+
+        final Token name;
+        final Expr value;
+    }
+
     static class Binary extends Expr {
         Binary(Expr left, Token operator, Expr right) {
-              this.left = left;
-              this.operator = operator;
-              this.right = right;
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
         }
 
         @Override
@@ -25,9 +43,10 @@ abstract class Expr {
         final Token operator;
         final Expr right;
     }
+
     static class Grouping extends Expr {
         Grouping(Expr expression) {
-              this.expression = expression;
+            this.expression = expression;
         }
 
         @Override
@@ -37,9 +56,10 @@ abstract class Expr {
 
         final Expr expression;
     }
+
     static class Literal extends Expr {
         Literal(Object value) {
-              this.value = value;
+            this.value = value;
         }
 
         @Override
@@ -49,10 +69,11 @@ abstract class Expr {
 
         final Object value;
     }
+
     static class Unary extends Expr {
         Unary(Token operator, Expr right) {
-              this.operator = operator;
-              this.right = right;
+            this.operator = operator;
+            this.right = right;
         }
 
         @Override
@@ -62,6 +83,19 @@ abstract class Expr {
 
         final Token operator;
         final Expr right;
+    }
+
+    static class Variable extends Expr {
+        Variable(Token name) {
+            this.name = name;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVariableExpr(this);
+        }
+
+        final Token name;
     }
 
     abstract <R> R accept(Visitor<R> visitor);
--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -22,6 +24,26 @@ class Interpreter implements Expr.Visitor<Object>,
         stmt.accept(this);
     }
 
+    void executeBlock(List<Stmt> statements,
+                      Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
         evaluate(stmt.expression);
@@ -33,6 +55,24 @@ class Interpreter implements Expr.Visitor<Object>,
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
     }
 
     @Override
@@ -59,6 +99,11 @@ class Interpreter implements Expr.Visitor<Object>,
 
         // Unreachable.
         return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
